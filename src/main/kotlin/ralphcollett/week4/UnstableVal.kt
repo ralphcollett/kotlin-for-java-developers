031db74e67bf0bd76a60d69392dd9f815eb7e9c8
ralph.collett <ralph.collett@gmail.com>
2021-11-06 04:46:05 +0000

    Unstable Val implementation.

---
+++ src/main/kotlin/ralphcollett/week4/UnstableVal.kt
@@ -0,0 +1,17 @@
+package ralphcollett.week4
+
+// Implement the property 'foo' so that it produced a different value on each access.
+// Note that you can modify the code outside the 'foo' getter
+// (e.g. add additional imports or properties).
+
+var counter: Int = 0
+
+val foo: Int
+    get() = counter++
+
+fun main(args: Array<String>) {
+    // The values should be different:
+    println(foo)
+    println(foo)
+    println(foo)
+}
