67e6bfaa0698ee5ffbe306ddc8469d8648b7a643
ralph.collett <ralph.collett@gmail.com>
2021-11-12 05:00:27 +0000

    Auxillary functions.

---
+++ src/main/kotlin/ralphcollett/week5/AuxillaryFunctions.kt
@@ -0,0 +1,32 @@
+package ralphcollett.week5
+
+// Simplify the following code using more appropriate auxiliary functions (with, let, run, apply, also).
+interface X {
+    var first: Int
+    var second: Int
+    var third: Int
+}
+
+interface Y {
+    fun start()
+    fun finish()
+}
+
+interface Z {
+    fun init()
+}
+
+fun foo(x: X, y: Y?, z: Z) {
+    with(x) {
+        first = 1
+        second = 2
+        third = 3
+    }
+    y?.run {
+        start()
+        finish()
+    }
+    val result = z.apply {
+        init()
+    }
+}
